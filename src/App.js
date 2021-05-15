ccb5beead8e4cbfd43ffec363a8ff970fa49abfc
juanabrate <juan.abrate21@gmail.com>
2021-05-15 00:14:56 -0300

    basic func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import axios from 'axios';
 import Search from './components/Search';
+import React, {useEffect, useState} from 'react';
+
+const configUrl = `https://api.themoviedb.org/3/configuration?api_key=4295c0e29a9f109077cc7792f1675b63`;
+console.log('config', axios(configUrl).then(res => {console.log(res)}));
 
 function App() {
+
+  const [config, setConfig] = useState();
+
+  useEffect(() => {
+    axios(configUrl).then(res => {setConfig(res.data)});
+    console.log('app', config);
+  }, []);
+
   return (
     <div className="App">
       
-      <Search/>
+      <Search config={config} />
       
       
       
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,36 +1,36 @@
 import axios from 'axios';
 import styled from 'styled-components';
 import React, {useState, useEffect} from "react";
+// import { NavLink } from 'react-router-dom';
 
 // console.log('bad req', axios('https://api.themoviedb.org/3/search/movie?api_key=4295c0e29a9f109077cc7792f1675b63&query=')
 // .then(res => {
 //     console.log(res)
 // }));
 
-export default function Search() {   
-
+export default function Search({ config }) {   
+    console.log(config)
     const [query, setQuery] = useState("");   
     const [loading, setLoading] = useState(false);
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=4295c0e29a9f109077cc7792f1675b63&query=${query}`;
+        const queryUrl = `https://api.themoviedb.org/3/search/movie?api_key=4295c0e29a9f109077cc7792f1675b63&query=${query}`;
+        // const imgUrl = `http://image.tmdb.org/t/p/w342${img_path}`;
 
         if (query != "") {
             setLoading(true);
-            axios(url)
+            axios(queryUrl)
                 .then(res => {
                     setLoading(false);
 
                     setMovies(res.data.results);
                     console.log(movies)
+                    console.log(res);
 
                 });
         }
-        
-        
-
     }, [query]);
 
 
@@ -42,7 +42,10 @@ export default function Search() {
             <div className='movies'>
                 
                 {query != "" ? movies.map((item) => 
-                    <p key={item.id}>{item.original_title}</p>
+                    <div style={{display:'flex', flexDirection:'row'}}>
+                        <p key={item.id}>{item.original_title}</p>
+                        <img src={`http://image.tmdb.org/t/p/w92${item.poster_path}`}/>
+                    </div>
                 ) : null}
 
             </div>
